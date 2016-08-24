98b49d418f13283a31780e6a81a6491b923a87e7
Andrea Frank & Gregory Berns-Leone <pair+andreafrank.gregoryBL@devbootcamp.com>
2016-08-24 18:52:51 -0500

    create teacher profile page

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -15,4 +15,8 @@ class Teacher < ApplicationRecord
   def school_key_is_correct
     errors.add(@school_key, "is not the correct school key") unless school_key.downcase == "go demogorgons"
   end
+
+  def full_name
+    [first_name, last_name].join(" ")
+  end
 end
