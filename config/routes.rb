694394fb0ce917b8530a8226019013dddabcf4d8
Lauren Daniels <laurenddaniels@gmail.com>
2016-08-24 18:01:01 -0500

    Started student index view

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   devise_for :teachers, path: 'auth'
 
+  get '/students/index', to: 'students#index'
+
   root to: 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
