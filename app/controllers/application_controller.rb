239abc55dcf1629f8d1a0ea4188a08d559c7d7bf
Andrea Frank & Gregory Berns-Leone <pair+andreafrank.gregoryBL@devbootcamp.com>
2016-08-24 17:44:56 -0500

    Add ability to register only with a valid school key

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:school_key])
+  end
+
+  # def sign_up_params
+  #   params.require(:teacher).permit(:first_name, :last_name, :school_key, :email, :password, :password_confirmation)
+  # end
+
+  # def account_update_params
+  #   params.require(:teacher).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password)
+  # end
 end
