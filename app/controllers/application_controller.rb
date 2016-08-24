09c4101772efabba5999cf1f1aedb20adfdbe470
Andrea Frank & Gregory Berns-Leone <pair+andreafrank.gregoryBL@devbootcamp.com>
2016-08-24 18:01:44 -0500

    add first and last name as fields on registration form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,14 +6,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:school_key])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:school_key, :first_name, :last_name])
   end
-
-  # def sign_up_params
-  #   params.require(:teacher).permit(:first_name, :last_name, :school_key, :email, :password, :password_confirmation)
-  # end
-
-  # def account_update_params
-  #   params.require(:teacher).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password)
-  # end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160824203843) do
+ActiveRecord::Schema.define(version: 20160824225343) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 20160824203843) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "first_name"
+    t.string   "last_name"
     t.index ["email"], name: "index_teachers_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_teachers_on_reset_password_token", unique: true, using: :btree
   end
