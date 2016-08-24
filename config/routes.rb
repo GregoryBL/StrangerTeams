968b747546ca984104860e1e95f9884bf130b112
Andrea Frank & Gregory Berns-Leone <pair+andreafrank.gregoryBL@devbootcamp.com>
2016-08-24 15:53:01 -0500

    add basic devise; switch files to haml

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
+  devise_for :teachers, path: 'auth'
 
-  root to: 'welcome#index'
+  root to: 'students#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
