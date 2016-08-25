8438cf8940a246cd3b316832bc13de83632a3d9e
Gregory Berns-Leone <gberns.l@gmail.com>
2016-08-24 22:29:36 -0500

    students table implemented so line uncommented

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,7 +1,6 @@
 class TeachersController < ApplicationController
   def show
     @teacher = Teacher.find(params[:id])
-    # TO DO! Uncomment line 5
-    # @students = @teacher.students
+    @students = @teacher.students
   end
 end
