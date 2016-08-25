a2c6b5278a8ee08d05cb721ddb4bf3329514cca5
Andrea Frank & Gregory Berns-Leone <pair+andreafrank.gregorybl@devbootcamp.com>
2016-08-25 10:35:20 -0500

    Add link and students in controller

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -5,8 +5,7 @@ class TeachersController < ApplicationController
 
   def show
     @teacher = Teacher.find(params[:id])
-    # TO DO! Uncomment line 5
-    # @students = @teacher.students
+    @students = @teacher.students
   end
 
   def destroy
