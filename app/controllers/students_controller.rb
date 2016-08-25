4ef0902383e020894e1a9316a7f5ff3fec65fc7c
Andrea Frank & Gregory Berns-Leone <pair+andreafrank.gregorybl@devbootcamp.com>
2016-08-25 09:51:58 -0500

    add authorization in student and teacher controllers

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,6 +2,8 @@ require 'csv'
 
 class StudentsController < ApplicationController
 
+  before_action :authenticate_teacher!
+
   def index # call method to sort students alphabetically
     @students = Student.sort_alphabetically_by_last_name
   end
