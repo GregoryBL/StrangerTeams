183a7ab360cf328445d30084297d4ce8e5ea67f5
Gregory Berns-Leone <gberns.l@gmail.com>
2016-08-26 14:06:57 -0500

    Add student id to form and csv maker

--- make_csv.rb
+++ make_csv.rb
@@ -9,14 +9,25 @@ def make_n_new_students(n = 300)
   gender_array = ["male", "female", "transgender", "other"]
   shirt_size = ["xs", "s", "m", "l", "xl"]
   allergy_array = ["peanuts", "eggs", "lactose"]
+  lettersNumbers = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".to_a
   r = Random.new
 
   student_data = (0..300).to_a.map do |ind|
-    [Faker::Name.first_name, Faker::Name.last_name, r.rand(6..8), gender_array.sample, r.rand(4.0).round(2), r.rand(10), shirt_size.sample, allergy_array.sample(r.rand(allergy_array.length)).join(" ")]
+    [
+      lettersNumbers.sample(12).join('')
+      Faker::Name.first_name,
+      Faker::Name.last_name,
+      r.rand(6..8),
+      gender_array.sample,
+      r.rand(4.0).round(2),
+      r.rand(10),
+      shirt_size.sample,
+      allergy_array.sample(r.rand(allergy_array.length)).join(" ")
+    ]
   end
 
   CSV.open(filename, "wb") do |csv|
-    csv << ["first_name", "last_name", "grade_level", "gender", "gpa", "detention_count", "shirt_size", "food_allergies"]
+    csv << ["student_id", "first_name", "last_name", "grade_level", "gender", "gpa", "detention_count", "shirt_size", "food_allergies"]
     student_data.each { |sd| csv << sd }
   end
 end
